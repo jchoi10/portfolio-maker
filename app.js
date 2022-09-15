fa8d11edc0864c5011d51c0a3b404236c8c71497
jchoi10 <noreply>
2022-09-15 00:37:21 -0400

    create html file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const name = profileDataArgs[0];
@@ -36,5 +38,8 @@ const generatePage = (name, github) => {
   `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
