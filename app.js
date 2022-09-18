77d65687ed4843a9807355b4ed15fccb6a92a072
jchoi10 <noreply>
2022-09-18 11:31:14 -0400

    update app.js clean

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
 const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', pageHTML, err => {
